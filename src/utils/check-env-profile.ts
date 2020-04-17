a82a911ff039b1be1912fa5de5244f13990b55f6
zhi.zhong <zhong.zhi@163.com>
2020-04-17 16:01:34 +0800

    fix: DeprecationWarning: Tapable.plugin is deprecated. Use new API on `.hooks` instead

--- src/utils/check-env-profile.ts
+++ src/utils/check-env-profile.ts
@@ -1,8 +1,6 @@
 const { profile, PROFILE } = process.env;
 process.env.PROFILE = profile || PROFILE;
 
-console.log('--process.env.PROFILE:', process.env.PROFILE);
-
 if (!process.env.PROFILE) {
   console.error(`
 The \`PROFILE\` environment variable is required but was not specified.
